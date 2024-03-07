587fa3cd78688a7495bbe4e2de5642c95271dab1
ZAMZAM ALI HEDAR <133240996+XAMZam@users.noreply.github.com>
2024-03-07 08:20:55 -0800

    control_flow dart

---
+++ control flow.dart
@@ -0,0 +1,12 @@
+void main() {
+  // greater, less, and equal numbers.
+  var num = 15;
+  
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
